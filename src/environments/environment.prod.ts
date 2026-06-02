4241b5288bc2aabd0c1d99519e17b759dc53e7e2
Andreus luigi martins <82423710+andreusmartins@users.noreply.github.com>
2026-06-02 18:01:40 -0300

    Update production API URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,3 @@
 export const environment = {
-  apiUrl: 'kodama.proxy.rlwy.net:37660'  // <-- substitua pela URL do Railway após o deploy
+  apiUrl: 'atendimento-production-bb60.up.railway.app'  // <-- substitua pela URL do Railway após o deploy
 };
